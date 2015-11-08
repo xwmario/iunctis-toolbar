00a0884caf552f1590fd065f9cf881c6ec96a3aa
iunctis <steven.abgrall@gmail.com>
2015-11-08 01:33:07 +0100

    Test
    test

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: iunctis-toolbar
+# about: Add a button to create a spoiler
+# version: 0.1
+# authors: Matthew Wilkin
+# url: https://github.com/iunctis/iunctis-toolbar
+
+enabled_site_setting :spoiler_ui_enabled
