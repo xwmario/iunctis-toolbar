1b2964259e27d0e27d5cf75830127ce16856896d
iunctis <steven.abgrall@gmail.com>
2016-03-06 00:34:55 +0100

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -4,4 +4,5 @@
 # authors: Matthew Wilkin et Steven
 # url: https://github.com/iunctis/iunctis-toolbar
 
+register_asset "javascripts/iunctis_bbcode.js", :server_side
 enabled_site_setting :iunctistlb_ui_enabled
